041b1ddc7e0666b9861af258fa9f86b8d6eaf1a1
nkgocz <859574881@qq.com>
2025-08-15 13:21:11 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ console.log("JavaScript Loaded!"); // 在浏览器控制台显示消息
 
 document.addEventListener('DOMContentLoaded', function() {
   // 页面加载完成后执行的代码
-  alert("welcome to Noah's Diary:D");
+  alert("USER IN LOGGED");
 });
 
 // 等待DOM加载完成
