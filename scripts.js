23932456e9e38adf845f61d77554c9836c1f9b64
nkgocz <859574881@qq.com>
2026-01-06 21:56:40 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -172,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function() {
             setTimeout(() => {
                 showScrollNotice();
             }, 40000);
-        }, 100000000);
+        }, 15000);
     }
     
     // 页面加载2秒后开始第一次滚动
